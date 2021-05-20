aa066ac2d662e7f5fe78f22fc97d507dc15f601c
viktorv9 <38245893+viktorv9@users.noreply.github.com>
2021-05-20 11:39:57 +0200

    wat error handling en documentatie

--- app/prediction/routes.py
+++ app/prediction/routes.py
@@ -7,7 +7,16 @@ from flask import jsonify
 @bp.route('', methods=['GET'])
 def getPrediction():
 
-    hours = int(request.args.get('hours')) # get query param 'hours', for amount of (hour based) predictions
+    try:
+        if 'hours' in request.args:
+            hours = int(request.args.get('hours')) # get query param 'hours', for amount of (hour based) predictions
+        else:
+            return "Missing query parameter 'hours'", 400
+    except ValueError:
+        return "Query parameter 'hours' needs an integer from 1 to 4", 400
+
+    if (hours > 4 or hours < 1):
+        return "Query parameter 'hours' needs an integer from 1 to 4", 400
 
     predictionController = PredictionController()
     results = predictionController.getProductionPrediction(hours)
