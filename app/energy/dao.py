6e858ff9b22b94e5d3c0a9c980ba68b4c6aaf053
Rowan-Paul <contact@rowanpaulflynn.com>
2021-05-20 10:45:01 +0200

    Fix hardcodedDate
    is now a currentData

--- app/energy/dao.py
+++ app/energy/dao.py
@@ -2,67 +2,69 @@ from app.core.db import get_db, get_rpi_db
 from datetime import date, datetime, timedelta, timezone
 import pandas as pd
 
+
 class EnergyDAO:
-  def __init__(self):
-    pass
+    def __init__(self):
+        pass
 
-  
-  def fetchConsumptionData(self):
-    table = 'energy_consumption'
-    fetch_query = 'SELECT time, P1 FROM %s'%table
+    def fetchConsumptionData(self):
+        table = 'energy_consumption'
+        fetch_query = 'SELECT time, P1 FROM %s' % table
 
-    db = get_db()
-    cursor = db.cursor()
-    cursor.execute(fetch_query)  # TODO: only fetch new data instead of everything
-    rows = cursor.fetchall()
+        db = get_db()
+        cursor = db.cursor()
+        cursor.execute(fetch_query)
+        rows = cursor.fetchall()
 
-    data = []
-    
-    hardcodedDate = '2021-04-05 11:00'
-    hardcodedDateFormat = datetime.strptime(hardcodedDate, "%Y-%m-%d %H:%M")
-    hardCodedDate_hours = hardcodedDateFormat + timedelta(hours=-4)
-    hardCodedDate_hoursFormat = hardCodedDate_hours.strftime('%Y-%m-%d %H:%M')
+        data = []
 
-    for row in rows:
-          consumptionDate = row[0];
-          consumptionDateFormat = datetime.fromtimestamp(consumptionDate)
-          consumptionDate_hours = consumptionDateFormat + timedelta(hours=2)
-          consumptionDate_hoursFormat = consumptionDate_hours.strftime('%Y-%m-%d %H:%M')
-          
-          if(consumptionDate_hoursFormat > hardCodedDate_hoursFormat):
-            englishFormat = consumptionDate_hours.strftime('%I:%M %p')
+        currentDate = date.today()
+        formattedDate = datetime.strptime(currentDate, "%Y-%m-%d %H:%M")
+        currentDate_hours = formattedDate + timedelta(hours=-4)
+        currentDate_hoursFormat = currentDate_hours.strftime('%Y-%m-%d %H:%M')
 
-            data.append({
-              'labels': englishFormat,
-              'values': row[1]
-            })
-    return data
+        for row in rows:
+            consumptionDate = row[0]
+            consumptionDateFormat = datetime.fromtimestamp(consumptionDate)
+            consumptionDate_hours = consumptionDateFormat + timedelta(hours=2)
+            consumptionDate_hoursFormat = consumptionDate_hours.strftime(
+                '%Y-%m-%d %H:%M')
 
-  def fetchData(self, type):
-    table = 'Grid' if type == 'consumption' else 'PV'
-    fetch_query = 'SELECT * FROM %s'%table
+            if(consumptionDate_hoursFormat > currentDate_hoursFormat):
+                englishFormat = consumptionDate_hours.strftime('%I:%M %p')
 
-    db = get_rpi_db()
-    cursor = db.cursor()
-    cursor.execute(fetch_query)  # TODO: only fetch new data instead of everything
-    rows = cursor.fetchall()
+                data.append({
+                    'labels': englishFormat,
+                    'values': row[1]
+                })
+        return data
 
-    data = []
-    for row in rows[:3]:
-        data.append(list(row))
+    def fetchData(self, type):
+        table = 'Grid' if type == 'consumption' else 'PV'
+        fetch_query = 'SELECT * FROM %s' % table
 
-    return data
+        db = get_rpi_db()
+        cursor = db.cursor()
+        # TODO: only fetch new data instead of everything
+        cursor.execute(fetch_query)
+        rows = cursor.fetchall()
 
-  def insertData(self, type, data):
-    table = 'energy_consumption' if type == 'consumption' else 'energy_production'
-    insert_query = 'INSERT INTO %s VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'%table
-      
-    db = get_db()
-    cursor = db.cursor()
+        data = []
+        for row in rows[:3]:
+            data.append(list(row))
 
-    for row in data:
-        var = (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9],
-                row[10], row[11], row[12], row[13], row[14], row[15], row[16], row[17], row[18], row[19], row[20])
-        cursor.execute(insert_query, var)
+        return data
 
-    return ''
+    def insertData(self, type, data):
+        table = 'energy_consumption' if type == 'consumption' else 'energy_production'
+        insert_query = 'INSERT INTO %s VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)' % table
+
+        db = get_db()
+        cursor = db.cursor()
+
+        for row in data:
+            var = (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9],
+                   row[10], row[11], row[12], row[13], row[14], row[15], row[16], row[17], row[18], row[19], row[20])
+            cursor.execute(insert_query, var)
+
+        return ''
