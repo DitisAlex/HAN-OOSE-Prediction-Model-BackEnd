46d3ca7b36049cfd2fbb890276fdec20a01bd76b
Rowan-Paul <contact@rowanpaulflynn.com>
2021-05-20 10:48:35 +0200

    Fix some things

--- app/energy/dao.py
+++ app/energy/dao.py
@@ -1,68 +1,69 @@
 from app.core.db import get_db, get_rpi_db
 from datetime import date, datetime, timedelta, timezone
 
+
 class EnergyDAO:
-  def __init__(self):
-    pass
+    def __init__(self):
+        pass
 
-  def fetchProductionData(self):
-      table = 'energy_production'
-      fetch_query = 'SELECT Time, P1 FROM %s'%table
+    def fetchProductionData(self):
+        table = 'energy_production'
+        fetch_query = 'SELECT Time, P1 FROM %s' % table
 
-      db = get_db()
-      cursor = db.cursor()
-      cursor.execute(fetch_query)  # TODO: only fetch new data instead of everything
-      rows = cursor.fetchall()
-      data = []
+        db = get_db()
+        cursor = db.cursor()
+        # TODO: only fetch new data instead of everything
+        cursor.execute(fetch_query)
+        rows = cursor.fetchall()
+        data = []
 
-      hardcodedDate = '2021-04-05 10:00'
-      hardcodedDateFormat = datetime.strptime(hardcodedDate, "%Y-%m-%d %H:%M")
-      hardCodedDate_hours = hardcodedDateFormat + timedelta(hours=-4)
-      hardCodedDate_hoursFormat = hardCodedDate_hours.strftime('%Y-%m-%d %H:%M')
+        currentDate = date.today()
+        formattedDate = datetime.strptime(currentDate, "%Y-%m-%d %H:%M")
+        currentDate_hours = formattedDate + timedelta(hours=-4)
+        currentDate_hoursFormat = currentDate_hours.strftime('%Y-%m-%d %H:%M')
 
-      for row in rows:
-        productionDate = row[0]
-        productionDateFormat = datetime.fromtimestamp(productionDate)
-        productionDate_hours = productionDateFormat + timedelta(hours=2)
-        productionDate_hoursFormat = productionDate_hours.strftime('%Y-%m-%d %H:%M')
-          
-        if(productionDate_hoursFormat > hardCodedDate_hoursFormat):
-            englishFormat = productionDate_hours.strftime('%I:%M %p')
+        for row in rows:
+            productionDate = row[0]
+            productionDateFormat = datetime.fromtimestamp(productionDate)
+            productionDate_hours = productionDateFormat + timedelta(hours=2)
+            productionDate_hoursFormat = productionDate_hours.strftime(
+                '%Y-%m-%d %H:%M')
 
-            data.append({
-              'labels': englishFormat,
-              'values': row[1]
-            })
-      return data
+            if(productionDate_hoursFormat > currentDate_hoursFormat):
+                englishFormat = productionDate_hours.strftime('%I:%M %p')
 
-  def fetchData(self, type):
-    table = 'Grid' if type == 'consumption' else 'PV'
-    fetch_query = 'SELECT * FROM %s'%table
+                data.append({
+                    'labels': englishFormat,
+                    'values': row[1]
+                })
+        return data
 
-    db = get_rpi_db()
-    cursor = db.cursor()
-    cursor.execute(fetch_query)  # TODO: only fetch new data instead of everything
-    rows = cursor.fetchall()
+    def fetchData(self, type):
+        table = 'Grid' if type == 'consumption' else 'PV'
+        fetch_query = 'SELECT * FROM %s' % table
 
-    data = []
-    for row in rows:
-        data.append(list(row))
+        db = get_rpi_db()
+        cursor = db.cursor()
+        # TODO: only fetch new data instead of everything
+        cursor.execute(fetch_query)
+        rows = cursor.fetchall()
 
-    return data
+        data = []
+        for row in rows:
+            data.append(list(row))
 
-  def insertData(self, type, data):
-    table = 'energy_consumption' if type == 'consumption' else 'energy_production'
-    insert_query = 'INSERT INTO %s VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'%table
-      
-    db = get_db()
-    cursor = db.cursor()
+        return data
 
-    for row in data:
-        var = (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9],
-                row[10], row[11], row[12], row[13], row[14], row[15], row[16], row[17], row[18], row[19], row[20])
-        cursor.execute(insert_query, var)
+    def insertData(self, type, data):
+        table = 'energy_consumption' if type == 'consumption' else 'energy_production'
+        insert_query = 'INSERT INTO %s VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)' % table
 
-    return ''
+        db = get_db()
+        cursor = db.cursor()
 
+        for row in data:
+            var = (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9],
+                   row[10], row[11], row[12], row[13], row[14], row[15], row[16], row[17], row[18], row[19], row[20])
+            cursor.execute(insert_query, var)
 
-
+        return ''
--- app/energy/routes.py
+++ app/energy/routes.py
@@ -4,22 +4,23 @@ from app.energy.controller import EnergyController
 
 energyController = EnergyController()
 
+
 @bp.route('/production', methods=['GET'])
 def getProduction():
     dat = energyController.getProductionData()
-    # return dat
+    
     return jsonify(dat)
 
 # Fetch Consumption data from Raspberry Pi and insert in database.
 @bp.route('/consumption/fetch', methods=['POST'])
 def fetchConsumptionData():
-    
     energyController.fetchConsumptionData()
+
     return 'Successfully fetched energy consumption data from the Raspberry Pi and insterted it into the database!'
 
 # Fetch Production data from Raspberry Pi and insert in database.
 @bp.route('/production/fetch', methods=['POST'])
 def fetchProductionData():
-    
     energyController.fetchProductionData()
+
     return 'Successfully fetched energy production data from the Raspberry Pi and insterted it into the database!'
