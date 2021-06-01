7647306b939e64edd7bf0439c5454dd83870e9a1
Bart de Kinkelaar <bar.dekinkelaar@student.han.nl>
2021-06-01 11:41:29 +0200

    Created exceptions for different error handling.

--- app/weather/controller.py
+++ app/weather/controller.py
@@ -2,6 +2,7 @@ from app.weather.dao import WeatherDAO
 from app.weather.domain import WeatherPoint
 from datetime import datetime, timedelta, timezone
 from pyowm.owm import OWM
+from flask import abort
 
 class WeatherController:
     def __init__(self):
@@ -9,21 +10,20 @@ class WeatherController:
         pass
 
     def insertWeatherData(self):
-
         # Setup weather, taken from Pytorch model.
         owm = OWM('1a4df9d4817c3d16e92b272d59531753')
         mgr = owm.weather_manager()
         one_call = mgr.one_call(lat=51, lon=5)
         current = one_call.current
         current.temperature('celsius')['temp']
-
+        
         weatherPoint = WeatherPoint(
-                datetime.fromtimestamp(current.ref_time).strftime('%Y-%m-%d %H:%M:%S'),
-                current.temperature('celsius')['temp'],
-                current.clouds,
-                current.wind()['speed'],
-                current.pressure['press'])
-
+            datetime.fromtimestamp(current.ref_time).strftime('%Y-%m-%d %H:%M:%S'),
+            current.temperature('celsius')['temp'],
+            current.clouds,
+            current.wind()['speed'],
+            current.pressure['press']
+        )
         self.weatherDAO.insertWeatherData(weatherPoint)
 
     def getWeatherData(self):
--- app/weather/routes.py
+++ app/weather/routes.py
@@ -1,3 +1,4 @@
+from typing import Type
 from flask import jsonify
 from app.weather import bp
 from app.weather.controller import WeatherController
@@ -5,9 +6,17 @@ from app.weather.controller import WeatherController
 from flask import jsonify
 
 
-@bp.route('/fetch', methods=['GET'])
+@bp.route('', methods=['POST'])
 def insertWeatherData():
-    c = WeatherController()
-    c.insertWeatherData()
 
-    return "Weather data inserted"
+    try:
+        c = WeatherController()
+        c.insertWeatherData()
+        return "Weather data inserted"
+    except TypeError:
+        return "Weather data not inserted: required arguments missing"
+    except AttributeError:
+        return "Weather data not inserted: no data found"
+
+    # except:
+    #     return "Weather data not inserted"
