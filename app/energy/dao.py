d1c8438db5893dc72be80734ad5a9ec97a4cc6e6
viktorv9 <38245893+viktorv9@users.noreply.github.com>
2021-06-03 09:36:32 +0200

    conflict fix 1/?

--- app/energy/dao.py
+++ app/energy/dao.py
@@ -45,35 +45,6 @@ class EnergyDAO:
             else:
                 abort(404, description = "No datas found")
 
-    def fetchEnergyData(self, type):
-        table = 'Grid' if type == 'consumption' else 'PV'
-        fetch_query = 'SELECT * FROM %s' % table
-
-        db = get_rpi_db()
-        cursor = db.cursor()
-        cursor.execute(fetch_query)
-        rows = cursor.fetchall()
-
-        data = []
-        for row in rows:
-            data.append(list(row))
-
-        return data
-
-    def insertEnergyData(self, type, data):
-        table = 'energy_consumption' if type == 'consumption' else 'energy_production'
-        insert_query = 'INSERT INTO %s VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)' % table
-
-        db = get_db()
-        cursor = db.cursor()
-
-        for row in data:
-            var = (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9],
-                   row[10], row[11], row[12], row[13], row[14], row[15], row[16], row[17], row[18], row[19], row[20])
-            cursor.execute(insert_query, var)
-
-        return ''
-
     def getDataForPrediction(self):
 
         db = get_db()
