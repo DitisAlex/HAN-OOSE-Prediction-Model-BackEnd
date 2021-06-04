53a31856dbdf1648c471a44d403228b2ec53b76c
iliass <ilivss@pm.me>
2021-06-04 11:26:21 +0200

    fixed 1 test

--- tests/prediction/test_prediction_controller.py
+++ tests/prediction/test_prediction_controller.py
@@ -1,92 +1,121 @@
+import pytest
+from datetime import datetime, timedelta
 from app.prediction.controller import PredictionController
 from app.core.db import get_db
 import pandas as pd
 
-# def test_getProductionPrediction(monkeypatch):
-#     # Arrange
 
-#     hours = 3
-
-#     # Mock dao functions.
-#     class Recorder(object):
-#         called1 = False
-#         called2 = False
-#         called3 = False
-
-#     def fake_getDataForPrediction(self): # This isn't great but since the function we got from Kaggle uses the raw result of the SQL query, this is the only way we can supply dummy data.
-#         Recorder.called1 = True
-
-#         db = get_db()
-#         query = '''SELECT * FROM energy_production ORDER BY time DESC LIMIT 24'''
-#         data = pd.read_sql_query(query, db)
-
-#         return data
-
-#     def fake_deleteNewerPredictions(self, currentTime):
-#         Recorder.called2 = True
-
-#     def fake_insertPrediction(self, predictionPoint):
-#         Recorder.called3 = True
-
-#     monkeypatch.setattr(
-#         'app.energy.dao.EnergyDAO.getDataForPrediction', fake_getDataForPrediction)
-
-#     monkeypatch.setattr(
-#         'app.prediction.dao.PredictionDAO.deleteNewerPredictions', fake_deleteNewerPredictions)
-        
-#     monkeypatch.setattr(
-#         'app.prediction.dao.PredictionDAO.insertPrediction', fake_insertPrediction)
-
-#     # Act
-#     predictionController = PredictionController()
-#     result = predictionController.getProductionPrediction(hours)
-    
-
-#     # Assert
-#     assert len(result) == hours
-#     assert Recorder.called1
-#     assert Recorder.called2
-#     assert Recorder.called3
-
-def test_getProductionPredictionNoPVData(monkeypatch):
+def test_getProductionPrediction(app, monkeypatch):
     # Arrange
+    hours=1
+    mock_result_makePrediction = [[5000], [6000], [3000], [4000]]  # Hardcoded
+    expected_result = (datetime.now() + timedelta(hours=2)).strftime('%H:%M:%S') + \
+        ' ' + (datetime.now() + timedelta(hours=2+1)
+               ).strftime('%H:%M:%S') + ' ' + str(mock_result_makePrediction[0][0])
 
-    hours = 3
+    # Mock functions
 
-
-    # Mock dao functions.
     class Recorder(object):
         called1 = False
         called2 = False
         called3 = False
+        called4 = False
 
-    def fake_getDataForPrediction(self):
+    # This isn't great but since the function we got from Kaggle uses the raw result of the SQL query, this is the only way we can supply dummy data.
+    def fake_loadModel(self):
         Recorder.called1 = True
-        return []
+
+    def fake_makePrediction(self, hours):
+        Recorder.called2 = True
+        return mock_result_makePrediction
 
     def fake_deleteNewerPredictions(self, currentTime):
-        Recorder.called2 = True
-
-    def fake_insertPrediction(self, predictionPoint):
         Recorder.called3 = True
 
+    def fake_insertPrediction(self, predictionPoint):
+        Recorder.called4 = True
+
     monkeypatch.setattr(
-        'app.energy.dao.EnergyDAO.getDataForPrediction', fake_getDataForPrediction)
+        'app.prediction.controller.PredictionController.loadModel', fake_loadModel)
+
+    monkeypatch.setattr(
+        'app.prediction.controller.PredictionController.makePrediction', fake_makePrediction)
 
     monkeypatch.setattr(
         'app.prediction.dao.PredictionDAO.deleteNewerPredictions', fake_deleteNewerPredictions)
-        
+
     monkeypatch.setattr(
         'app.prediction.dao.PredictionDAO.insertPrediction', fake_insertPrediction)
 
-
     # Act
-    predictionController = PredictionController()
-    result = predictionController.getProductionPrediction(hours)
-    
+    with app.app_context():
+        pc = PredictionController()
+        result = pc.getProductionPrediction(hours)
+        print('\nTEST\n')
+        print(result[0])
 
     # Assert
+    assert len(result) == hours
     assert Recorder.called1
-    assert not Recorder.called2
-    assert not Recorder.called3
+    assert Recorder.called2
+    assert Recorder.called3
+    assert Recorder.called4
+    assert expected_result == str(result[0])
+
+
+def test_getProductionPredictionNoPVData(app, monkeypatch):
+    # Arrange
+    hours = 1
+    mock_result_makePrediction = [[5000], [6000], [3000], [4000]]  # Hardcoded
+    expected_result = (datetime.now() + timedelta(hours=2)).strftime('%H:%M:%S') + \
+                      ' ' + (datetime.now() + timedelta(hours=2 + 1)
+                             ).strftime('%H:%M:%S') + ' ' + str(mock_result_makePrediction[0][0])
+
+    # Mock functions
+
+    class Recorder(object):
+        called1 = False
+        called2 = False
+        called3 = False
+        called4 = False
+
+    # This isn't great but since the function we got from Kaggle uses the raw result of the SQL query, this is the only way we can supply dummy data.
+    def fake_loadModel(self):
+        Recorder.called1 = True
+
+    def fake_makePrediction(self, hours):
+        Recorder.called2 = True
+        return mock_result_makePrediction
+
+    def fake_deleteNewerPredictions(self, currentTime):
+        Recorder.called3 = True
+
+    def fake_insertPrediction(self, predictionPoint):
+        Recorder.called4 = True
+
+    monkeypatch.setattr(
+        'app.prediction.controller.PredictionController.loadModel', fake_loadModel)
+
+    monkeypatch.setattr(
+        'app.prediction.controller.PredictionController.makePrediction', fake_makePrediction)
+
+    monkeypatch.setattr(
+        'app.prediction.dao.PredictionDAO.deleteNewerPredictions', fake_deleteNewerPredictions)
+
+    monkeypatch.setattr(
+        'app.prediction.dao.PredictionDAO.insertPrediction', fake_insertPrediction)
+
+    # Act
+    with app.app_context():
+        pc = PredictionController()
+        result = pc.getProductionPrediction(hours)
+        print('\nTEST\n')
+        print(result[0])
+
+    # Assert
     assert len(result) == 0
+    assert Recorder.called1
+    assert Recorder.called2
+    assert Recorder.called3
+    assert Recorder.called4
+
